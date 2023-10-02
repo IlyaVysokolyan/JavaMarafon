9991d056a5aef68af74eb59309b45cbca1bfc1ca
Ilya <rd2d@list.ru>
2023-10-02 20:59:21 +0300

    Day 1

---
+++ src/day1/Task6.java
@@ -0,0 +1,14 @@
+package day1;
+
+public class Task6 {
+
+    public static void main(String[] args) {
+        int k = 4;
+        for (int i = 1; i < 11; i++) {
+            System.out.println(i + " х " + k + " = " + i * k);
+        }
+    }
+
+}
+
+
